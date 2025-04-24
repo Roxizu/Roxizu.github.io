f56a8e0d7fdd34f241abb808ab36cf68d68b2eaf
Roxizu <roxannebv22@gmail.com>
2025-04-24 09:43:18 -0400

    ultimos arreglos

--- js/permissions.js
+++ js/permissions.js
@@ -3,9 +3,17 @@ function checkPermissions() {
         .then(response => response.json())
         .then(data => {
             const addProductBtn = document.querySelector('.add-product-btn');
+            const viewProductsBtn = document.querySelector('.view-products-btn');
+            
             if (addProductBtn) {
                 if (!data.logged_in || data.rol !== 'empresa') {
                     addProductBtn.style.display = 'none';
+                }
+            }
+            
+            if (viewProductsBtn) {
+                if (!data.logged_in || data.rol !== 'empresa') {
+                    viewProductsBtn.style.display = 'none';
                 }
             }
         });
